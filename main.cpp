7c4ccc11821a714b6d4784f7f6bcb27435b92dd3
danielaghidini <daniela.ghidini@gmail.com>
2026-08-23 23:13:17 -0300

    Add Skyrim 1.7.99 Address Library support

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ extern "C" {
 
 		0,
 		SKSEPluginVersionData::kVersionIndependent_StructsPost629,
-		{ RUNTIME_VERSION_1_6_1130, 0 },
+		{ MAKE_EXE_VERSION(1, 7, 99), 0 },
 
 		0,	// works with any version of the script extender. you probably do not need to put anything here
 	};
